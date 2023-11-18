374513bedfbd3698492bf0a060f790c54a45cc17
rafal <17829904+rafal@users.noreply.github.com>
2023-11-18 21:54:57 +0100

    implement PostListItem component

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,40 +1,16 @@
 <script setup>
-import data from '@/data.json'
-import { ref } from 'vue'
+import PostListItem from '@/components/PostListItem.vue'
 const { posts = () => [] } = defineProps({
   posts: {
     type: Array,
     required: true
   }
 })
-const users = ref(data.users)
-const postById = (id) => posts.find((p) => p.id === id)
-const userById = (id) => users.value.find((u) => u.id === id)
 </script>
 
 <template>
   <div class="post-list">
-    <div v-for="post in posts" :key="post.id" class="post">
-      <div class="user-info">
-        <a href="#" class="user-name">{{ userById(post.userId).name }}</a>
-
-        <a href="#">
-          <img class="avatar-large" :src="userById(post.userId).avatar" alt="" />
-        </a>
-
-        <p class="desktop-only text-small">107 posts</p>
-      </div>
-
-      <div class="post-content">
-        <div>
-          <p>
-            {{ post.text }}
-          </p>
-        </div>
-      </div>
-
-      <div class="post-date text-faded">{{ postById(post.id).publishedAt }}</div>
-    </div>
+    <PostListItem v-for="post in posts" :key="post.id" :post="post" />
   </div>
 </template>
 
---
+++ src/components/PostListItem.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+  post: {
+    type: Object,
+    required: true,
+    default: () => {}
+  }
+})
+import data from '@/data.json'
+import { ref } from 'vue'
+const users = ref(data.users)
+const posts = ref(data.posts)
+const postById = (id) => posts.value.find((p) => p.id === id)
+const userById = (id) => users.value.find((u) => u.id === id)
+</script>
+
+<template>
+  <div class="post">
+    <div class="user-info">
+      <a href="#" class="user-name">{{ userById(post.userId).name }}</a>
+
+      <a href="#">
+        <img class="avatar-large" :src="userById(post.userId).avatar" alt="" />
+      </a>
+
+      <p class="desktop-only text-small">107 posts</p>
+    </div>
+
+    <div class="post-content">
+      <div>
+        <p>
+          {{ post.text }}
+        </p>
+      </div>
+    </div>
+
+    <div class="post-date text-faded">{{ postById(post.id).publishedAt }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
