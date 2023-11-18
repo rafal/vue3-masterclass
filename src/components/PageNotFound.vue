cf518713b85d9c3f70ada24f379b702bc82a67c6
rafal <17829904+rafal@users.noreply.github.com>
2023-11-18 20:55:16 +0100

    implement PageNotFound component

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div class="col-full text-center">
+    <h1>Not Found</h1>
+    <router-link :to="{ name: 'home' }">Read some cool threads</router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/components/PageHome.vue'
+import data from '@/data.json'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,25 @@ const router = createRouter({
       path: '/thread/:id',
       name: 'threadShow',
       props: true,
-      component: () => import('@/components/PageThreadShow.vue')
+      component: () => import('@/components/PageThreadShow.vue'),
+      beforeEnter(to, from, next) {
+        const threadExists = data.threads.find((thread) => thread.id === to.params.id)
+        if (!threadExists) {
+          next({
+            name: 'NotFound',
+            params: { pathMatch: to.path.substring(1).split('/') },
+            query: to.query,
+            hash: to.hash
+          })
+        } else {
+          next()
+        }
+      }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('@/components/PageNotFound.vue')
     }
   ]
 })
