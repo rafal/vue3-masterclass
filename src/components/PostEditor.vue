be5a423f356bc977b35fa3b2e4c8c87189e08c11
rafal <17829904+rafal@users.noreply.github.com>
2023-11-19 00:29:38 +0100

    implement post editor

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+const emit = defineEmits(['save'])
+const text = ref('')
+
+const save = () => {
+  const postId = 'gggg' + Math.random()
+  const post = {
+    id: postId,
+    userId: 'rpbB8C6ifrYmNDufMERWfQUoa202',
+    text: text.value,
+    publishedAt: Math.floor(Date.now() / 1000)
+  }
+  emit('save', post)
+  text.value = ''
+}
+</script>
+
+<template>
+  <div class="col-full">
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea v-model="text" id="" class="form-input" name="" rows="10" cols="30"></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,6 +2,7 @@
 import data from '@/data.json'
 import { ref, computed } from 'vue'
 import PostList from '@/components/PostList.vue'
+import PostEditor from '@/components/PostEditor.vue'
 const { id } = defineProps({
   id: {
     type: String,
@@ -13,11 +14,21 @@ const posts = ref(data.posts)
 
 const thread = computed(() => threads.value.find((t) => t.id === id))
 const threadPosts = computed(() => posts.value.filter((p) => p.threadId === id))
+
+const addPost = (e) => {
+  const post = {
+    ...e,
+    threadId: id
+  }
+  posts.value.push(post)
+  thread.value.posts.push(post.id)
+}
 </script>
 
 <template>
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostList :posts="threadPosts" />
+    <PostEditor :thread="thread" @save="addPost" />
   </div>
 </template>
