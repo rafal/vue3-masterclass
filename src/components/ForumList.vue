18cba40b53522350c3af11e5db8af65c08fdab8e
rafal <17829904+rafal@users.noreply.github.com>
2023-11-19 16:56:10 +0100

    create ForumList component

---
+++ src/components/ForumList.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+  forums: {
+    type: Array,
+    required: true,
+    default: () => []
+  }
+})
+
+const forumThreadsWord = (forum) => {
+  const threads = forum.threads?.length
+  if (!threads) return 'No threads'
+  return threads === 1 ? 'thread' : 'threads'
+}
+</script>
+
+<template>
+  <div class="col-full">
+    <div class="forum-list">
+      <h2 class="list-title">
+        <a href="#">Forums</a>
+      </h2>
+
+      <div class="forum-listing" v-for="forum in forums" :key="forum.id">
+        <div class="forum-details">
+          <router-link class="text-xlarge" :to="{ name: 'forum', params: { id: forum.id } }">{{
+            forum.name
+          }}</router-link>
+          <p>{{ forum.description }}</p>
+        </div>
+
+        <div class="threads-count">
+          <p>
+            <span class="count">{{ forum.threads?.length }}</span>
+            {{ forumThreadsWord(forum) }}
+          </p>
+        </div>
+
+        <div class="last-thread"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
