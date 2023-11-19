c9e9d8b13ee53468789f51066f1519fb20815b5f
rafal <17829904+rafal@users.noreply.github.com>
2023-11-19 16:56:28 +0100

    display ForumList instead of ThreadList on homepage

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,9 +1,9 @@
 <script setup>
-import ThreadList from '@/components/ThreadList.vue'
+import ForumList from '@/components/ForumList.vue'
 import data from '@/data.json'
 </script>
 
 <template>
   <h1>Welcome to the forum!</h1>
-  <ThreadList :threads="data.threads" />
+  <ForumList :forums="data.forums" />
 </template>
