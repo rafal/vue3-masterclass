8a6ae5f2df6025f58534c43d08439a9cfb138f47
rafal <17829904+rafal@users.noreply.github.com>
2023-11-20 01:41:24 +0100

    create CategoryList

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ForumList from '@/components/ForumList.vue'
+import source from '@/data.json'
+defineProps({
+  categories: {
+    type: Array,
+    required: true,
+    default: () => []
+  }
+})
+const forums = (category) => source.forums.filter((f) => f.categoryId === category.id)
+</script>
+
+<template>
+  <ForumList
+    v-for="category in categories"
+    :key="category.id"
+    :forums="forums(category)"
+    :category="category"
+  />
+</template>
+
+<style lang="scss" scoped></style>
